34494cfebbf79de8fecd2727e556e313defbd36a
utsaho <pipul.boddyo@gmail.com>
2025-08-04 11:23:07 +0600

    deploy

--- src/app/out-impressive-stats.tsx
+++ src/app/out-impressive-stats.tsx
@@ -37,7 +37,7 @@ const STATS = [
 
 export function OutImpressiveStats() {
   return (
-    <section className="px-8 pt-80">
+    <section className="px-8 mt-[400px]">
       <div className="container mx-auto text-center lg:text-left">
         <div className="grid place-items-center text-center">
           <Typography variant="h2" color="blue-gray" className="mb-2 text-4xl">
